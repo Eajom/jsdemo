85e864f9f2035ff2184f6195429b7d127b87667b
Masasa <jomvuu@gmail.com>
2022-08-03 19:39:29 +0300

    newest

--- demo.js
+++ demo.js
@@ -1,17 +1,19 @@
 
-(function () {
+(function speak (name) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 	var firstLetter = names[i].charAt(0).toLowerCase();
+  var speakWord='Good Bye ';
 
 	if (firstLetter === 'j') {
    
-    console.log('Bye '+firstLetter.toUpperCase()+names[i].slice(1));
+    console.log(speakWord+firstLetter.toUpperCase()+names[i].slice(1));
 
   } else {
+    var speakWord='Hello '
    
-    console.log( 'Hello '+(names[i]));
+    console.log( speakWord+(names[i]));
   }
 }
 
